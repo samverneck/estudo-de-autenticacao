794e2889221f47f1e579f2781493c07a0c3a7cf7
samverneck <nodezeirojs@gmail.com>
2017-06-21 17:26:55 -0300

    estudos add component

--- angular2auth/src/app/app.module.ts
+++ angular2auth/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
